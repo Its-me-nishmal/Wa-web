b4e2b9d50c755498318f8bae0f6b8261b366cb4e
Muhammad Nishmal P <138182452+Its-me-nishmal@users.noreply.github.com>
2024-03-18 05:15:28 +0000

    ok

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const app = express();
 const port = process.env.PORT || 5000;
-const sessionName = "Nishmal";
+const sessionName = "./Nishmal";
 const donet = "https://saweria.co/sansekai";
 const {
   default: sansekaiConnect,
